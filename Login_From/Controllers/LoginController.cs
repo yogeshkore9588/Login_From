ca3cb5c810578399d01bb131e69e8211a46746ae
KORE <KORE@LAPTOP-FI1733H9>
2024-12-16 01:43:00 +0530

    Add project files.

---
+++ Login_From/Controllers/LoginController.cs
@@ -0,0 +1,61 @@
+﻿using Login_From.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login_From.Controllers
+{
+    public class LoginController : Controller
+    {
+        SignUpLoginEntities db = new SignUpLoginEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(user u)
+        {
+            var user = db.users.Where(model => model.username == u.username && model.password == u.password).FirstOrDefault();
+            if(user != null)
+            {
+                Session["UserId"] = u.Id.ToString();
+                Session["Username"] = u.username.ToString();
+                TempData["LoginSuccessMessage"] = "<script>alert ('Login Successfully..!')</script>";
+                return RedirectToAction("Index", "User");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "<script>alert ('Username or Password is incorrect')</script>";
+                return View();
+            }
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(user u)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.users.Add(u);
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    ViewBag.InsertMessage = "<script>alert ('Registered Successfully..!')</script>";
+                    ModelState.Clear();
+                }
+                else
+                {
+                    ViewBag.InsertMessage = "<script>alert ('Registeration Failed..!')</script>";
+
+                }
+            }
+            return View();
+        }
+    }
+}
